ea366de7c1b3f8d4f1f09b3b8a2b3d3fb5a13d5b
Matteo Paier <matteo.paier.1997@gmail.com>
2021-01-13 22:44:26 +0100

    Fully themed

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
+function randomPastel() {
+  return "hsl(" + ~~(360 * Math.random()) + ",65%,75%)"
+}
+
 function choose(choices) {
-  var index = Math.floor(Math.random() * choices.length);
-  return choices[index];
+  var index = Math.floor(Math.random() * choices.length)
+  return choices[index]
 }
 
 window.addEventListener('load', () => {
+  document.body.style.backgroundColor = randomPastel()
   document.querySelector('#title').innerHTML = choose(['No', 'Non ancora', 'Ancora no'])
 })
