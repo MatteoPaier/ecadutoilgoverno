a89d42bb549cda0b93f4fc4425074f74f41034dd
Matteo Paier <matteo.paier.1997@gmail.com>
2021-01-13 22:59:56 +0100

    Added more reasons

--- main.js
+++ main.js
@@ -7,7 +7,20 @@ function choose(choices) {
   return choices[index]
 }
 
+const reasons = [
+  'No',
+  'Non ancora',
+  'Ancora no',
+  'Ha stato Renzi',
+  'Ha stato Renzie',
+  'Io non l\'ho toccato',
+  'Sciok',
+  'Shock',
+  'Era già rotto',
+  'Italia morta'
+]
+
 window.addEventListener('load', () => {
   document.body.style.backgroundColor = randomPastel()
-  document.querySelector('#title').innerHTML = choose(['No', 'Non ancora', 'Ancora no'])
+  document.querySelector('#title').innerHTML = choose(reasons)
 })
